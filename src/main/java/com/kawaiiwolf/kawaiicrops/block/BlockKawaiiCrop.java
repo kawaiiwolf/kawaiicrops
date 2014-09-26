7f8da09d0957e6f848b26f42cb3b00042518a661
Wolf <Wolf@Work>
2014-09-26 15:50:35 -0400

    Work Checkin

--- src/main/java/com/kawaiiwolf/kawaiicrops/block/BlockKawaiiCrop.java
+++ src/main/java/com/kawaiiwolf/kawaiicrops/block/BlockKawaiiCrop.java
@@ -9,12 +9,76 @@ import net.minecraft.block.material.Material;
 
 public class BlockKawaiiCrop extends Block {
 	
+	public enum EnumRenderType { CROSS, HASH }
+	
+	// Don't register if false
+	public boolean Enabled = false;
+	
+	// Number of crop stages
+	public int CropStages = 8;
+
+	// Render Type, # or X
+	public EnumRenderType RenderType = EnumRenderType.HASH;
+	
+	// Max block height
+	public int MaxHeight = 1;
+	
+	// Prevent lower blocks from hitting Meta 8 before the top
+	public boolean MaxHeightRequiredToRipen = true;
+	
+	// Restore blocks to earlier Meta state on harvest
+	public boolean MultiHarvest = false;
+	
+	// Meta to restore to if MultiHarvest is set
+	public int UnripeMeta = 4;
+	
+	// Hardness of unripe block
+	public float UnripeHardness = 0.5f;
+	
+	// Effectiveness of bonemeal.
+	public int BoneMealMin = 1;
+	public int BoneMealMax = 1;
+	
+	/* TODO: Drops
+
+	drop tables
+	grows on
+	growth rate
+
+	Number of drops
+	bonus seeds
+	seed return
+
+	water ?
+	seed ?
+	seededible ?
+	seedhunger ?
+	seedsaturation ?
+	Mystery Seed Weight
+
+	seed tooltip
+	fruit ?
+	fruit hunger
+	fruit saturation
+	fruit tooltip
+	*/
+	
+	
 	public BlockKawaiiCrop(String cropName)
 	{
 		super(Material.rock);
 		this.setBlockName(Constants.MOD_ID + "." + cropName );
 		this.setBlockTextureName(Constants.MOD_ID + ":" + cropName);
 		
+		
+		
+		
+		
+		
+	}
+	
+	public void register()
+	{
 		GameRegistry.registerBlock(this, this.getUnlocalizedName());
 	}
 }
--- src/main/java/com/kawaiiwolf/kawaiicrops/lib/ConfigurationLoader.java
+++ src/main/java/com/kawaiiwolf/kawaiicrops/lib/ConfigurationLoader.java
@@ -12,15 +12,14 @@ public class ConfigurationLoader {
 
 	
 	public static final String GENERAL_CROP_COMMENT = "" +
-			"Here you'll list the names of all the plants that you want the mod to generate."+"\n"+
-			"Make sure each crop name is lower case and has no spaces or punctuation. You"+"\n"+
-			"can separate these with commas or spaces. Once this list is completed, a new"+"\n"+
-			"entry will be created in the blocks.cfg file to hold all the details for this"+"\n"+
-			"plant. Plants must appear in this list to be included in the mod."+"\n"+
-			""+"\n"+
-			"Bad Name: Snow Peas"+"\n"+
-			"Good Name: snowpeas"+"\n"+
-			""+"\n"+
+			"Here you'll list the names of all the plants that you want the mod to generate. Make sure each crop\n"+
+			"name is lower case and has no spaces or punctuation. You can separate these with commas or spaces.\n"+
+			"Once this list is completed, a new entry will be created in the blocks.cfg file to hold all the\n"+
+			"details for this plant. Plants must appear in this list to be included in the mod.\n"+
+			"\n"+
+			"Bad Name: Snow Peas\n"+
+			"Good Name: snowpeas\n"+
+			"\n"+
 			"example: \"snowpea tomato broccoli\"";
 	
 	public static void loadConfiguration(String configFolder) {
@@ -41,7 +40,7 @@ public class ConfigurationLoader {
 		 * - For each crop, loadBlock() and register.
 		 */
 		cfg_general.setCategoryComment("KawaiiCrop Crops", GENERAL_CROP_COMMENT);
-		String cropsRaw = cfg_general.getString("Crops", "KawaiiCrop Crops", "snowpea tomato broccoli","Crop List");
+		String cropsRaw = cfg_general.getString("Crops", "KawaiiCrop Crops", "snowpea","Crop List");
 		String[] cropsParsed = cropsRaw.toLowerCase().replaceAll("[^a-z, ]", "").replaceAll("  ", " ").replaceAll(",,", ",").split("[, ]");
 		
 		for (int i = 0; i < cropsParsed.length; i++)
@@ -53,12 +52,25 @@ public class ConfigurationLoader {
 		
 		for (int i = 0; i < 4; i++) configs[i].save();
 	}
-	
+
 	private static BlockKawaiiCrop loadBlock(Configuration config, String name) {
 		
+		String category = "KawaiiCrops: " + name;
+		BlockKawaiiCrop b = new BlockKawaiiCrop(name);
 		
+		// Get crop variables from config file
+		b.Enabled = config.getBoolean("Enabled", category, false, "Is this a block in minecraft ? Defaults to false to allow you to configure before putting it in game.");
+		b.CropStages = config.getInt("CropStates", category, b.CropStages, 2, 8, "Number of crop states ?  Valid values are between 2 and 8. (Ex: Carrots = 4, Wheat = 8)");
+		b.RenderType = (config.getString("RenderType", category, "Hash", "How will the crop render ? Valid values are 'Hash' (Ex: Carrots) or 'Cross' (Ex: Mushrooms)").toLowerCase() == "cross" ? BlockKawaiiCrop.EnumRenderType.CROSS : BlockKawaiiCrop.EnumRenderType.HASH);
+		b.MaxHeight = config.getInt("MaxHeight", category, b.MaxHeight, 1, 32, "How many blocks tall will this crop grow ?");
+		b.MaxHeightRequiredToRipen = config.getBoolean("MaxHeightRequired", category, b.MaxHeightRequiredToRipen, "Does the plant need to be at max height before lower blocks are ready to harvest ?");
+		b.MultiHarvest = config.getBoolean("MultiHarvest", category, b.MultiHarvest, "Upon harvesting this crop, does it grow back to an earlier, unripe state ?");
+		b.UnripeMeta = config.getInt("UnripeMeta", category, b.UnripeMeta, 0, 7, "If MultiHarvest, upon harvesting the crop goes from Metadata value 7 to Meta ?");
+		b.UnripeHardness = config.getFloat("UnripeHardness", category, b.UnripeHardness, 0.0f, 1.0f, "Hardness of unripe crops (0 breaks instantly. Set higher to prevent accidental harvests) ?");
+		b.BoneMealMin = config.getInt("BoneMealMin", category, b.BoneMealMin, 0, 8, "Minimum stages of growth when using bonemeal.");
+		b.BoneMealMax = config.getInt("BoneMealMax", category, b.BoneMealMax, 0, 8, "Maximum stages of growth when using bonemeal.");
 		
-		return (name == null || name.length() == 0 ? null : new BlockKawaiiCrop(name)); 
+		return (name == null || name.length() == 0 ? null : b); 
 	}
 	
 	private static Item loadItem(Configuration config, String name){
